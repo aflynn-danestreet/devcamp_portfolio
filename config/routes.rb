4a74384e291524489edd0279e0c4c4c685f37d67
awf825 <faiden454@gmail.com>
2020-01-28 20:21:42 -0500

    customized portfolio routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  resources :portfolios
-  get 'about', to: 'pages#about'
+  resources :portfolios, except: [:show] #I want you on all resources cept show
+  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show' # line 6 in portfolio/index.html.erb
+  get 'about-me', to: 'pages#about'
   # you can pass anything after the CRUD action, this is customization
   get 'contact', to: 'pages#contact'
   resources :blogs
