1b095af05054caf569d5b5a7669e451b9bf70ff6
Aiden Flynn <aflynn@danestreet.com>
2020-04-05 20:30:58 -0400

    N.B: Gem is not ready for publication: method still implemented in the code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,18 @@ class ApplicationController < ActionController::Base
   include SetSource
   include CurrentUserConcern
   include DefaultPageContent
+
+  before_action :set_copyright
+
+  def set_copyright
+    @copyright = FlynnViewTool::Renderer.copyright "Aiden Flynn", "All rights reserved"
+  end
+end
+
+module FlynnViewTool
+  class Renderer
+    def self.copyright name, msg
+      "&copy; #{Time.now.year} | <b>#{name}</b> #{msg}".html_safe
+    end
+  end
 end
