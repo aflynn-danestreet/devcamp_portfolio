d2dac202608f057e687145595eedf798e5890599
Aiden Flynn <aflynn@danestreet.com>
2020-04-08 17:08:40 -0400

    Implemeted persistent position updates for drag and drop interface

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
-  resources :portfolios, except: [:show] #I want you on all resources cept show
+  resources :portfolios, except: [:show] do
+    # pass in path, "whenever you see portfolios/sort, I want you to do something else"
+    put :sort, on: :collection
+  end
   get 'angular-items', to: 'portfolios#angular'
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show' # line 6 in portfolio/index.html.erb
   get 'about-me', to: 'pages#about'
