9f65ee5b31930c663f7d912b4a228ca37ae297eb
Aiden Flynn <aflynn@danestreet.com>
2020-04-17 13:28:41 -0400

    Implemented redis library and created comment generator

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,6 +16,7 @@
 //= require rails-ujs
 //= require jquery-ui
 //= require html.sortable
+//= require cable
 //= require turbolinks
 //= require_tree .
 //= require cocoon
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :comments
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
   resources :portfolios, except: [:show] do
     # pass in path, "whenever you see portfolios/sort, I want you to do something else"
